a8ecfb767935a2ec3c590d18532cdc337e5a9076
Soumya Ranjan <50011453+Soumyaranjan1996@users.noreply.github.com>
2021-07-29 21:21:48 +0530

    Update tree.js

--- activity/command/tree.js
+++ activity/command/tree.js
@@ -1,5 +1,34 @@
 function treeFn(dirPath){
-    console.log("Tree command executed with " + dirPath);
+    if(dirPath == undefined){
+        console.log("Kindly enter the path");
+        return;
+    } else{
+        let doesExist = fs.existsSync(dirPath);
+        if(doesExist){
+            
+            treeHelper(dirPath, ""); 
+            
+        }else{
+            console.log("Kindly enter the correct path");
+            return;
+        }
+    }
+}
+
+function treeHelper(dirPath,indent){
+    let isFile = fs.lstatSync(dirPath).isFile();
+    if(isFile == true){
+        let fileName = path.basename(dirPath);
+        console.log(indent + "├──" + fileName);
+    }else{
+        let dirName = path.basename(dirPath);
+        console.log(indent + "└──" + dirName);
+        let childrens = fs.readdirSync(dirPath);
+        for(let i = 0;i < childrens.length;i++){
+            let childPath = path.join(dirPath,childrens[i]);
+            treeHelper(childPath,indent + "\t");
+        }
+    }
 }
 
 //code export
